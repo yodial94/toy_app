132e564a2e049c8f63e8bfe05a3bb8cb1de7c1e5
yodial94 <alghiffaari17@gmail.com>
2021-01-14 18:04:09 +0700

    remove error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
 	has_many :microposts
-	validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
-  	validates FILL_IN, presence: true    # Replace FILL_IN with the right code.
 end
