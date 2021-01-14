14ea46b96107821c793463d4b9291ae48f62bc30
yodial94 <alghiffaari17@gmail.com>
2021-01-14 14:57:34 +0700

    name and email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	
+  resources :users
 	root 'application#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
